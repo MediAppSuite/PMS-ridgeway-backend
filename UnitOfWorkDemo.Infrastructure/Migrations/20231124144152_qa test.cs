c614ab6e419b097d1b9585d33d41d58a959e5bd4
thisara-medi <thisarabandara.medi@gmail.com>
2023-11-24 20:19:07 +0530

    add migration

---
+++ UnitOfWorkDemo.Infrastructure/Migrations/20231124144152_qa test.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PMS.Infrastructure.Migrations
+{
+    public partial class qatest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PatientTypeID",
+                table: "PatientRecord");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "BHTNumber",
+                table: "PatientRecord",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Diagnosis",
+                table: "PatientRecord",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Diagnosis",
+                table: "PatientRecord");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "BHTNumber",
+                table: "PatientRecord",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PatientTypeID",
+                table: "PatientRecord",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
